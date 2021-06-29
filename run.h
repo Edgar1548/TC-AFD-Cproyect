61b9d5d63acab71e63c7ded964e066ff7cf67d5c
paulozterra <66433825+paulozterra@users.noreply.github.com>
2021-06-29 12:54:09 -0500

    Update run.h

--- run.h
+++ run.h
@@ -12,27 +12,45 @@ void run(){
     int ind_inicial;
     int n_final;
     int* f_states;
-
-    do{
-        cin >> n >> ind_inicial >> n_final;
-    }while(n < ind_inicial || n < n_final || n<=0);
-    
-    f_states = new int[n_final];
-    for (int i=0;i<n_final;i++){
-        cin >> f_states[i];
-    }
-    AFD afd(n, ind_inicial, n_final, f_states);
     int qini;
     int caracter;
     int qfin;
-    for (int i=0; i<2*n; i++){
-        do{
-            cin >> qini >> caracter >> qfin;
-            }while(caracter!=1 and caracter!=0);
-        
-        afd.crear_conecction(qini, caracter, qfin);
+    int stat=1;
+    int stat2=1;
+
+    cin >> n >> ind_inicial >> n_final;
+
+    if(n < ind_inicial || n < n_final || n<=0){
+        stat*=0;
     }
-    afd.MIN();
+    if(stat==1){
+        f_states = new int[n_final];
+        for (int i=0;i<n_final;i++){
+            cin >> f_states[i];
+        }
+        AFD afd(n, ind_inicial, n_final, f_states);
+        for (int i=0; i<2*n; i++){
+            cin >> qini >> caracter >> qfin;
+            if(caracter!=1 and caracter!=0){
+                stat2*=0;
+
+                cout<<endl<<"error en la linea: "<<i+1<<endl;
+            }
+            if(stat2==1){
+                afd.crear_conecction(qini, caracter, qfin);
+            }
+            else{
+                break;
+            }
+        }
+        if(stat==1 and stat2==1){
+            afd.MIN();
+        }
+    }
+    else{
+        cout<<endl<<"Error en la linea 0"<<endl;
+    }
+
 }
 
 #endif //AFD_PROYECT_RUN_H
