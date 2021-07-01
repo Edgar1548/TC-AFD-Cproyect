cd0900c341e14c77c1c7306a8385f4f7aa9e1aa0
Edgar Chacon <edgar.chacon@utec.edu.pe>
2021-06-30 23:57:08 -0500

    Comments

--- AFD.cpp
+++ AFD.cpp
@@ -61,13 +61,11 @@ pair<vector<int>, string> AFD::hallar_0(pair<vector<int>, string> STR){
             pq.push(it); // Guardamos en el heap
         }
     }
-
     while (!pq.empty()){  // Pasamos del heap al vector (heapsort)
         vec.push_back(pq.top());
-        cadena += to_string(pq.top());
+        cadena += to_string(pq.top()) + " "; //Se agrego " " para manejar el error
         pq.pop();
     }
-
     pair<vector<int>,string>pair_; // Guardamos en un pair
     pair_ = {vec, cadena};
     return pair_;
@@ -89,7 +87,7 @@ pair<vector<int>, string> AFD::hallar_1(pair<vector<int>, string> STR){
     }
     while (!pq.empty()){ // pasamos del heap al vector (heapsort)
         vec.push_back(pq.top());
-        name += to_string(pq.top());
+        name += to_string(pq.top()) + " "; //Se agrego " " para manejar el error
         pq.pop();
     }
     pair<vector<int>,string>pair_; // Guardamos en un pair
@@ -104,10 +102,10 @@ void AFD::MIN() {
     // con todos los estados unitario
     vec.reserve(n_states); // Reservamos el tamaño del vec a n (todos los estados unitarios)
     pair<vector<int>,string> pair_; // Representa al estado con todos los estados unitario
-    for(int i=0; i<n_states; i++){ //Recorremos el array con estados unitarios
+    for(int i=n_states-1; i>=0; i--){ //Recorremos el array con estados unitarios
         int tmp = array[i]->position;
         vec.push_back(tmp);
-        sup += to_string(tmp);
+        sup += to_string(tmp) + " "; // Se agrego " " para arreglar el error
     }
     pair_ = {vec, sup}; // Guardamos
     if (MINSINC(pair_, cadena)){ // Si MINSINC retorna verdad es sincronizable, caso contrario no
@@ -118,12 +116,11 @@ void AFD::MIN() {
     else cout << "NO";
 }
 
-
 bool AFD::MINSINC(pair<vector<int>, string> STA, string &cadena){
     if (STA.first.size()==1){
         cadena = to_string(0);
         return true;
-    }//Si el tamaño de la cadena es 1,
+    } //Si el tamaño de la cadena es 1,
     pair<vector<int>, string> state_0; // Pair que guarda el estado al que se dirigue con 0
     pair<vector<int>, string> state_1; // Pair que guarda el estado al que se dirigue con 1
     queue<pair<vector<int>, string>> que; // Queue que guarda un vector y string, ambos representan un estado
@@ -136,6 +133,7 @@ bool AFD::MINSINC(pair<vector<int>, string> STA, string &cadena){
         if (STA.first.size()==1) break; // Si el estado es de tamaño 1, es unitario, y se rompe el while
         state_0 = hallar_0(STA); // Funcion para hallar el camino por 0
         state_1 = hallar_1(STA); // Funcion para hallar el camino por 1
+        map2[STA.second] = true;
         if(map2.find(state_0.second) == map2.end()) { //Verifica que el camino por 0 no haya sido recorrido
             map[state_0.second] = {STA.second,'0'}; //Guardamos el Padre de state_0  y el camino '0'
             map2[state_0.second] = true; //state_0 sera recorrido
@@ -143,13 +141,14 @@ bool AFD::MINSINC(pair<vector<int>, string> STA, string &cadena){
         }
         if(map2.find(state_1.second)==map2.end()) {
             map[state_1.second] = {STA.second,'1'}; //Guardamos el Padre de state_1  y el camino '1'
-            map2[state_1.second] = true; //state_0 sera recorrido
+            map2[state_1.second] = true; //state_1 sera recorrido
             que.push(state_1); // Pusheamos al queue el state_0
         }
         que.pop(); // Hacemos pop del front del queue
     }
     if (que.empty()) return false; // Si es vacio el queue, habra recorrido el automata potencia
     // y no encontro ningun camino a un automata unitario
+
     while (map.find(STA.second)!=map.end()){ //Recorremos desde el automata unitario hasta el automata con todos los estados unitario
         cadena += map[STA.second].second; // Le sumamos a cadena la coneccion (simbolo del alfabeto)
         STA.second = map[STA.second].first; //  Actualizamos el STA
@@ -160,6 +159,5 @@ bool AFD::MINSINC(pair<vector<int>, string> STA, string &cadena){
         cadena[i] = *--it;
         *it = first;
     }// Recorremos el string a la inversa
-
     return true;
 }
